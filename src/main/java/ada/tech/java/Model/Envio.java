50b0ef2ffe6242bd719fffd09a3e5beab7ee3573
vivianpanizzi <105569477+vivianpanizzi@users.noreply.github.com>
2024-04-25 20:20:01 -0300

    Atualizações no Model e Repository

--- src/main/java/ada/tech/java/Model/Envio.java
+++ src/main/java/ada/tech/java/Model/Envio.java
@@ -1,4 +1,49 @@
 package ada.tech.java.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Envio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idenvio;
+    private long idCliente;
+    private long idCompra;
+    private String remetente;
+    private String enderecoRemetente;
+    private String destinatario;
+    private String enderecoDestinatatio;
+    private String cidade;
+    private String estado;
+    private String pais;
+    private String cep;
+
+
+    public Envio(long idCliente, long idCompra, String remetente, String enderecoRemetente, String destinatario,
+                 String enderecoDestinatatio, String cidade,
+                 String estado, String pais, String cep) {
+        this.idCliente = idCliente;
+        this.idCompra = idCompra;
+        this.remetente = remetente;
+        this.enderecoRemetente = enderecoRemetente;
+        this.destinatario = destinatario;
+        this.enderecoDestinatatio = enderecoDestinatatio;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.pais = pais;
+        this.cep = cep;
+    }
+
+
+    public Envio() {
+
+    }
 }
--- src/main/java/ada/tech/java/Service/CadastrarEnvioService.java
+++ src/main/java/ada/tech/java/Service/CadastrarEnvioService.java
@@ -1,5 +1,7 @@
 package ada.tech.java.Service;
 
+import ada.tech.java.Model.Envio;
+import ada.tech.java.Repository.EnvioRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -9,9 +11,13 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class CadastrarEnvioService {
 
-    //get idcliente
-    //get idcompra
+private final EnvioRepository envioRepository;
 
+private final Envio envio;
+    public CadastrarEnvioService(EnvioRepository envioRepository, Envio envio) {
+        this.envioRepository = envioRepository;
+        this.envio = envio;
+    }
 
 
 
