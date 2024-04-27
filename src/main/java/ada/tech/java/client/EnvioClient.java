b47d469635bcc46d2b77d911ed23570c1a6a7bcd
Gabriela Visani <gabriela.visani@aztronic.com.br>
2024-04-27 11:49:18 -0300

    adicionei classes de config e client

---
+++ src/main/java/ada/tech/java/client/EnvioClient.java
@@ -0,0 +1,10 @@
+package ada.tech.java.client;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
+import ada.tech.java.client.payload.ItemResponse;
+
+public interface EnvioClient {
+    @GetExchange(value = "/query/{idEnvio}")
+    ItemResponse consultarEnvioProduto(@PathVariable(value = "idEnvio") String sku);
+}
